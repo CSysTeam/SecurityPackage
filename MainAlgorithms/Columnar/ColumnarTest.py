3bd080fd3647b104c2966c03933d0bab54a5f92e
sara <saraadel.fcis2012@gmail.com>
2017-03-20 15:21:36 +0200

    fix bug in unit test files

--- MainAlgorithms/Columnar/ColumnarTest.py
+++ MainAlgorithms/Columnar/ColumnarTest.py
@@ -29,49 +29,49 @@ class ColumnarTest(unittest.TestCase):
         algorithm = Columnar()
         cipher = algorithm.Encrypt(self.mainPlain1, self.mainkey)
         # Add x's or not
-        assertTrue(cipher == self.mainCipher1 or cipher == self.mainCipher2)
+        self.assertTrue(cipher == self.mainCipher1 or cipher == self.mainCipher2)
 
     def test_ColumnarTestDec1(self):
         algorithm = Columnar()
         plain1 = algorithm.Decrypt(self.mainCipher1, self.mainkey)
         plain2 = algorithm.Decrypt(self.mainCipher2, self.mainkey)
 
-        assertTrue(plain1 == self.mainPlain1 or plain2 == self.mainPlain2)
+        self.assertTrue(plain1 == self.mainPlain1 or plain2 == self.mainPlain2)
 
     def test_ColumnarTestAnalysis1(self):
         algorithm = Columnar()
         key1 = algorithm.Analyse(self.mainPlain1, self.mainCipher1)
         key2 = algorithm.Analyse(self.mainPlain2, self.mainCipher2)
         for i in range(len(self.mainkey)):
-            assertTrue(self.mainkey[i] == key1[i] or self.mainkey[i] == key2[i])
+            self.assertTrue(self.mainkey[i] == key1[i] or self.mainkey[i] == key2[i])
 
     def test_ColumnarTestEnc2(self):
         algorithm = Columnar()
         cipher = algorithm.Encrypt(self.mainPlain3, self.mainkey1)
-        assertEqual(cipher, self.mainCipher3)
+        self.assertEqual(cipher, self.mainCipher3)
 
     def test_ColumnarTestEnc3(self):
         algorithm = Columnar()
         cipher = algorithm.Encrypt(self.mainPlain3, self.mainkey2)
 
-        assertTrue(cipher == self.mainCipher4 or cipher == self.mainCipher5)
+        self.assertTrue(cipher == self.mainCipher4 or cipher == self.mainCipher5)
     def test_ColumnarTestDec2(self):
         algorithm = Columnar()
         plain = algorithm.Decrypt(self.mainCipher3, self.mainkey1)
-        assertEqual(plain, self.mainPlain3)
+        self.assertEqual(plain, self.mainPlain3)
 
     def test_ColumnarTestDec3(self):
         algorithm = Columnar()
         plain1 = algorithm.Decrypt(self.mainCipher4, self.mainkey2)
         plain2 = algorithm.Decrypt(self.mainCipher5, self.mainkey2)
-        assertTrue(plain1 ==  self.mainPlain3 or plain2 == self.mainPlain4)
+        self.assertTrue(plain1 ==  self.mainPlain3 or plain2 == self.mainPlain4)
 
     def test_ColumnarTestAnalysis2(self):
         algorithm = Columnar()
         key = algorithm.Analyse(self.mainPlain3, self.mainCipher3)
 
         for i in range(len(self.mainkey1)):
-            assertEqual(self.mainkey1[i], key[i])
+            self.assertEqual(self.mainkey1[i], key[i])
 
     def test_ColumnarTestAnalysis3(self):
         algorithm = Columnar()
@@ -79,23 +79,23 @@ class ColumnarTest(unittest.TestCase):
         key2 = algorithm.Analyse(self.mainPlain4, self.mainCipher5)
 
         for i in range(len(self.mainkey2)):
-            assertTrue(self.mainkey2[i] == key1[i] or self.mainkey2[i] == key2[i])
+            self.assertTrue(self.mainkey2[i] == key1[i] or self.mainkey2[i] == key2[i])
 
     def test_ColumnarNewTestEnc(self):
         algorithm = Columnar()
         cipher = algorithm.Encrypt(self.newPlain, self.newKey)
-        assertEqual(cipher, self.newCipher)
+        self.assertEqual(cipher, self.newCipher)
 
     def test_ColumnarNewTestDec(self):
         algorithm = Columnar()
         plain1 = algorithm.Decrypt(self.newCipher, self.newKey)
-        assertEqual(plain1, self.newPlain)
+        self.assertEqual(plain1, self.newPlain)
 
     def test_ColumnarNewTestAnalysis(self):
         algorithm = Columnar()
         key1 = algorithm.Analyse(self.newPlain, self.newCipher)
         for i in range(len(self.newKey)):
-            assertEqual(self.newKey[i], key1[i])
+            self.assertEqual(self.newKey[i], key1[i])
 
 
 if __name__ == '__main__':
--- MainAlgorithms/RailFence/RailFenceTest.py
+++ MainAlgorithms/RailFence/RailFenceTest.py
@@ -21,50 +21,50 @@ class RailFenceTest(unittest.TestCase):
     def test_RailFenceTestEnc1(self):
         algorithm = RailFence()
         cipher = algorithm.Encrypt(self.mainPlain1, self.mainKey)
-        assertTrue(cipher, self.mainCipher)
+        self.assertTrue(cipher, self.mainCipher)
 
     def test_RailFenceTestDec1(self):
         algorithm = RailFence()
         plain = algorithm.Decrypt(self.mainCipher, self.mainKey)
-        assertTrue(plain, self.mainPlain1)
+        self.assertTrue(plain, self.mainPlain1)
 
     def test_RailFenceTestAnalysis1(self):
         algorithm = RailFence()
         key = algorithm.Analyse(self.mainPlain1, self.mainCipher)
-        assertEqual(self.mainKey, key)
+        self.assertEqual(self.mainKey, key)
 
     def test_RailFenceTestEnc2(self):
         algorithm = RailFence()
         cipher = algorithm.Encrypt(self.mainPlain1, self.mainKey2)
         # Add x's or not
-        assertTrue(cipher == self.mainCipher2 or cipher == self.mainCipher3)
+        self.assertTrue(cipher == self.mainCipher2 or cipher == self.mainCipher3)
 
     def test_RailFenceTestDec2(self):
         algorithm = RailFence()
         plain1 = algorithm.Decrypt(self.mainCipher2, self.mainKey2)
         plain2 = algorithm.Decrypt(self.mainCipher3, self.mainKey2)
-        assertTrue(plain1 == self.mainPlain1 or plain2 ==  self.mainPlain2)
+        self.assertTrue(plain1 == self.mainPlain1 or plain2 ==  self.mainPlain2)
 
     def test_RailFenceTestAnalysis2(self):
         algorithm = RailFence()
         key = algorithm.Analyse(self.mainPlain1, self.mainCipher2)
         key2 = algorithm.Analyse(self.mainPlain1, self.mainCipher3)
-        assertTrue(self.mainKey2 == key or self.mainKey2 == key2)
+        self.assertTrue(self.mainKey2 == key or self.mainKey2 == key2)
 
     def test_RailFenceTestNewEnc(self):
         algorithm = RailFence()
         cipher = algorithm.Encrypt(self.newPlain, self.newkey)
-        assertTrue(cipher, self.newCipher)
+        self.assertTrue(cipher, self.newCipher)
 
     def test_RailFenceTestNewDec(self):
         algorithm = RailFence()
         plain = algorithm.Decrypt(self.newCipher, self.newkey)
-        assertTrue(plain, self.newPlain)
+        self.assertTrue(plain, self.newPlain)
 
     def test_RailFenceTestNewAnalysis(self):
         algorithm = RailFence()
         key = algorithm.Analyse(self.newPlain, self.newCipher)
-        assertEqual(self.newkey, key)
+        self.assertEqual(self.newkey, key)
 
 
 if __name__ == '__main__':
